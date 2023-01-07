e16cabcbc9ae6d0d33d816814d83815ae10e3a76
James Coleby <32794776+decoleby91@users.noreply.github.com>
2023-01-07 20:14:28 +0000

    Added base comments to the file

---
+++ index.js
@@ -0,0 +1,13 @@
+// Grab elements
+
+//Nav styles on scroll
+
+// Open menu & search pop-up
+
+// Open/Close search form pop-up
+
+// -- Close the search form pop-up on ESC keypress
+
+// Switch theme/add to local storage
+
+// Swiper
